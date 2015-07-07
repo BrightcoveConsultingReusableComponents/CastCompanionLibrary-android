7ff7b5f28f51d8ff0fb79cf1e0fb7deec3bdebea
Kate <firekesti@users.noreply.github.com>
2015-07-07 17:46:18 -0400

    Update VideoCastControllerFragment.java

--- src/com/google/android/libraries/cast/companionlibrary/cast/player/VideoCastControllerFragment.java
+++ src/com/google/android/libraries/cast/companionlibrary/cast/player/VideoCastControllerFragment.java
@@ -821,7 +821,8 @@ public class VideoCastControllerFragment extends Fragment implements
             }
         }
         mCastManager.setActiveTrackIds(tracksArray);
-        /*if (tracks.size() > 0) {
+        /* This is comented out because the CCL was interfering with our own styling logic.
+        if (tracks.size() > 0) {
             mCastManager.setTextTrackStyle(mCastManager.getTracksPreferenceManager()
                     .getTextTrackStyle());
         }*/
